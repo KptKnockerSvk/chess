1a7d60544626eeda25bc1e2c9d9606573dce4795
Adam Franko1 <adamfranko753@gmail.com>
2023-05-29 15:55:43 +0200

    Add separate funx for pawn

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def creation_of_field():
                 ["__"], ["__"], ["__"], ["__"], ["__"], ["__"], ["__"], ["__"],    # 5
                 ["__"], ["__"], ["__"], ["__"], ["__"], ["__"], ["__"], ["__"],    # 6 
                 ["__"], ["__"], ["__"], ["__"], ["__"], ["__"], ["__"], ["__"]]    # 7
-                     # A0  B1  C2  D3  E4  F5  G6  H7    
-    
+                    # A0  B1  C2  D3  E4  F5  G6  H7    
+                    # 97  98  99  100  101 102 103 104
     figure_transformation(board_in, all_figs_in, 1)
     return board_in
 
@@ -79,37 +79,46 @@ def figure_movement(choice, num):
         figure_transformation(board_out, ret_list)
         pass
 
-def pawn_movement():
-    pass
+def pawn_movement():      
+    print("Your pawns are:\n", PAWNS,"\n")
+    pos = "a2" # input("str-Which one: ") #
+
+    #Check for enemy
+    x = ord(pos[0]) - 1
+    y = ord(pos[0]) + 1
+    print(x,y)
+    if x == 96:
+        h_list = chr(y) + str(int(pos[1]) + 1)
+    elif y == 105:
+        h_list = chr(x) + str(int(pos[1]) + 1)
+    else:
+        pass
+    print(h_list)
+    pos_list = [[]]
+    while True:
+        if int(pos[1]) == 2:
+            double_step = 1 #int(input("1 or 2 ")) - 1 #
+        else:
+            double_step = 0
+        pos_list[0] = pos
+        pos_index = PAWNS.index(pos_list)
+        board_out[8 * (8 - int(pos[1])) + pos_index] = ["__"]
+        PAWNS[pos_index] = pos[0] + str(int(pos[1]) + 1 + double_step)
+        pos_list[0] = pos[0] + str(int(pos[1]) + double_step)
+        return pos_list
 
 
 
 #Initialisation
 if __name__ == "__main__":
     board_out = creation_of_field()
-    while True:        
-        #(current_board(board_out))
-
+    while True:      
         fig = "p" #(input("str-Which figure do you want to move: P - awn " + "\n")) # 
-        fig = fig.lower()
-        if fig == "p":
-            print("Your pawns are:\n", PAWNS,"\n")
-            pos = "h2" # input("str-Which one: ") #
-            pos_list = [[]]
-            while True:
-                if int(pos[1]) == 2:
-                    double_step = 1 #int(input("1 or 2 ")) - 1 #
-                else:
-                    double_step = 0
-                pos_list[0] = pos
-                pos_index = PAWNS.index(pos_list)
-                board_out[8 * (8 - int(pos[1])) + pos_index] = ["__"]
-                PAWNS[pos_index] = pos[0] + str(int(pos[1]) + 1 + double_step)
-                pos_list[0] = pos[0] + str(int(pos[1]) + double_step)
-                break
-            
-            
+        fig = fig.lower()    
+
+        if fig == "p":                         
+            pos_list = pawn_movement()   
         
         figure_movement(fig, pos_list)         
-        (current_board(board_out))
+        #(current_board(board_out))
         break
